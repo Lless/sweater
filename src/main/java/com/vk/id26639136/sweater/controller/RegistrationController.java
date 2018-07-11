4a852c9345a9d1e1b4e29fb232a710190205cdd8
Konkistador <Konkistador.rus@gmail.com>
2018-07-11 07:57:08 +0300

    Registration confirmation with email

--- src/main/java/com/vk/id26639136/sweater/controller/RegistrationController.java
+++ src/main/java/com/vk/id26639136/sweater/controller/RegistrationController.java
@@ -1,20 +1,20 @@
 package com.vk.id26639136.sweater.controller;
 
-import com.vk.id26639136.sweater.domain.Role;
 import com.vk.id26639136.sweater.domain.User;
-import com.vk.id26639136.sweater.repos.UserRepo;
+import com.vk.id26639136.sweater.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import java.util.Collections;
 import java.util.Map;
 
 @Controller
 public class RegistrationController {
     @Autowired
-    private UserRepo userRepo;
+    private UserService userService;
 
     @GetMapping("/registration")
     public String registration() {
@@ -22,16 +22,20 @@ public class RegistrationController {
     }
 
     @PostMapping("/registration")
-    public String addUser(User user, Map<String,Object> model) {
-        User userFromDb = userRepo.findByUsername(user.getUsername());
-        if (userFromDb != null) {
-            model.put("message","User exists!");
+    public String addUser(User user, Map<String, Object> model) {
+        if (!userService.addUser(user)) {
+            model.put("message", "User exists!");
             return "registration";
         }
-        user.setActive(true);
-        user.setRoles(Collections.singleton(Role.USER));
-        userRepo.save(user);
+
         return "redirect:/login";
     }
 
+    @GetMapping("/activate/{code}")
+    public String activate(Model model, @PathVariable String code) {
+        boolean isActivated = userService.activateUser(code);
+        String message = isActivated? "User successfully activated" : "Activation code not found!";
+        model.addAttribute("message", message);
+        return "login";
+    }
 }
